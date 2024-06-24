3e94057b91af9b9658d9f96a8ca91b5e6bd84f93
Amen <53493383+logmoon@users.noreply.github.com>
2024-06-24 18:23:09 +0100

    Delete Nyx/src/game/sandbox directory

--- Nyx/src/game/sandbox/main_scene.h
+++
@@ -1,7 +0,0 @@
-# pragma once
-# include "../../engine/types.h"
-
-void main_scene_init(void);
-void main_scene_update(f32 time, f32 delta_time);
-void main_scene_render(void);
-void main_scene_shutdown(void);
